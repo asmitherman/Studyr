6ec4ea1ede7de7927b96e3e79ed6c1c4a3ae8c3f
Austin Smitherman <asmitherman@wlan-t-172-122-dhcp.CSUChico.EDU>
2017-09-11 20:06:14 -0700

    Created welcome + generic login page

--- Studyr/src/app/app.module.ts
+++ Studyr/src/app/app.module.ts
@@ -2,7 +2,8 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-
+import { WelcomePage } from '../pages/welcome/welcome';
+import { LoginPage } from '../pages/login/login';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
@@ -10,12 +11,15 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { SignaturePadModule } from 'angular2-signaturepad';
 
 @NgModule({
   declarations: [
     MyApp,
     AboutPage,
     ContactPage,
+    WelcomePage,
+    LoginPage,
     HomePage,
     TabsPage
   ],
@@ -28,6 +32,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     MyApp,
     AboutPage,
     ContactPage,
+    WelcomePage,
+    LoginPage,
     HomePage,
     TabsPage
   ],
--- Studyr/src/pages/home/home.ts
+++ Studyr/src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { WelcomePage } from '../welcome/welcome';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,13 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  logout(){
+        // Remove API token
+        this.navCtrl.push(WelcomePage);
+        // const root = this.app.getRootNav();
+        // root.popToRoot();
   }
 
 }
